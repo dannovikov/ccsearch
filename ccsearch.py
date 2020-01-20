eabefdaff5ef32dcfb10e1f5b4ff1f3228e9b3a7
Dan <dnovikov@optonline.net>
2020-01-20 13:15:32 -0500

    refactor

--- ccsearch.py
+++ ccsearch.py
@@ -1,4 +1,5 @@
-import datetime
+import sys
+from datetime import timedelta
 from youtube_transcript_api import YouTubeTranscriptApi as yt
 
 #Get YT Link
@@ -10,54 +11,49 @@ def getVideoID(link):
     if '?t=' in link:
         link = link.split('?')[-2]
     if '&' in link:
-        link = link.split('&')[0] 
+        link = link.split('&')[0]
     if 'watch?v=' in link:
         link = link.split('watch?v=')[-1]
     if 'youtu.be' in link:
         link = link[-11:]
     return link
 
-#link= getLink(link)
-#vid_id = getVideoID(link)
-
 # Get transcript
-
 def getCC(vid_id):
     try:
         captions = yt.get_transcript(vid_id, languages=['en'])
         for dictionary in captions:
             dictionary['text'] = dictionary['text'].lower()
-        
+
         return captions
     except:
         return [{'text': 'None'}]
         # quit()
 
-        
+
 # Get Search Term
 #search_term = input('Please enter your search term: ').lower()
 
+#rewrite find method to create a list
 def find(search_term, vid_id, captions):
     notFound = True
-    with open('templates/results.txt', 'w'):
-        pass
+    results = []
+    for dictionary in captions:
+        if dictionary['text'] == 'None':
+            results.append("NC")
 
-    with open('templates/results.txt', 'a') as f:
-        for dictionary in captions:
-            if dictionary['text'] == 'None':
-                f.write('Unfortunately, there are no captions associated with this video.')
-                notFound = False
-            elif search_term in dictionary['text']:
-                f.write(f'{search_term} found in: ')
-                f.write('<br>')
-                f.write(f'Text: {dictionary["text"]}')
-                f.write('<br>')
-                seconds = int(dictionary['start'])
-                f.write(f'Time stamp: {str(datetime.timedelta(seconds=seconds))}')
-                f.write('<br>')
-                f.write(f'Link to timestamp: https://youtu.be/{vid_id}?t={seconds}')
-                f.write('<br><br><br>')
-                notFound = False
-        if notFound:
-            f.write('Search term was not matched to any captions in this video. Try a smaller search term.')
+        elif search_term in dictionary['text']:
+            notFound = False
 
+            # 0. appearedin, 1.timestamp, 2.link to timestamp
+            seconds = int(dictionary['start'])
+            results.append([dictionary['text'],
+                            str(timedelta(seconds=seconds)),
+                            f"https://youtu.be/{vid_id}?t={seconds}",])
+        elif notFound:
+            notFound = True
+
+    if notFound:
+        results.append('NF')
+
+    return results
